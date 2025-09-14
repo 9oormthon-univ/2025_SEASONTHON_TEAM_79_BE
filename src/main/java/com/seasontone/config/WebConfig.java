7098e8462093b85ab0f460955591b2116e07cb4c
sunkon25 <kcindy325k@kyonggi.ac.kr>
2025-09-14 20:26:28 +0900

    feat: Cors 추가

--- src/main/java/com/seasontone/config/WebConfig.java
+++ src/main/java/com/seasontone/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 				.allowedOriginPatterns("*")
-				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"); // 허용할 HTTP method
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH"); // 허용할 HTTP method
 //				.allowCredentials(true); // 쿠키 인증 요청 허용
 	}
 }
