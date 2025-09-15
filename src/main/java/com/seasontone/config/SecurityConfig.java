891e10dadf7494d725cc31982e48d066cbaa972a
yunji1014 <7787yunji@gmail.com>
2025-09-16 08:58:11 +0900

    임시접근

--- src/main/java/com/seasontone/config/SecurityConfig.java
+++ src/main/java/com/seasontone/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
 								"/swagger-ui.html"
 						).permitAll()
 						.requestMatchers("/users/**").permitAll()
-						.anyRequest().authenticated()
+						.anyRequest().permitAll()
 
 				)
 				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
