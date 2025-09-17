b2f8c4c38dcc23ac035df052f9fe4d3617ab403a
yunji1014 <7787yunji@gmail.com>
2025-09-17 22:22:28 +0900

    fix/SecurityConfig의 checklist get 인증없이

--- src/main/java/com/seasontone/config/SecurityConfig.java
+++ src/main/java/com/seasontone/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.seasontone.jwt.JwtExceptionFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -50,6 +51,7 @@ public class SecurityConfig {
 										"/swagger-ui/**",
 										"/swagger-ui.html"
 								).permitAll()
+								.requestMatchers(HttpMethod.GET,"/api/**").permitAll()
 								.requestMatchers("/users/**").permitAll()
 								.anyRequest().authenticated()
 
--- src/main/java/com/seasontone/dto/checklists/ChecklistCreateRequest.java
+++ src/main/java/com/seasontone/dto/checklists/ChecklistCreateRequest.java
@@ -1,7 +1,6 @@
 package com.seasontone.dto.checklists;
 
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 public record ChecklistCreateRequest(
     @NotNull Long userId,
